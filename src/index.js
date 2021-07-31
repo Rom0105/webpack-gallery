662d6ba7605c354c26cbdb1b3aeab2e9273b4ad7
Rom0105 <savosko.photo@gmail.com>
2021-07-31 17:42:31 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import "./css/common.css";
+import galleryItems from "./gallery";
+import createImagesList from "./markup";
 
-class Game {
-  name = "Violin Charades";
+const ulListImages = document.querySelector(".js-gallery");
+const cardImages = createImagesList(galleryItems);
+ulListImages.insertAdjacentHTML("beforeend", cardImages);
+const modal = document.querySelector(".js-lightbox");
+ulListImages.addEventListener("click", imageContainerClick);
+const imageOpenModal = document.querySelector(".lightbox__image");
+const overlayRef = document.querySelector(".lightbox__overlay");
+
+const closeModal = document.querySelector(".lightbox__button");
+closeModal.addEventListener("click", closeModalClick);
+
+window.addEventListener("keyup", closeModalClickEscape);
+
+overlayRef.addEventListener("click", onBackDropClick);
+
+function imageContainerClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  modal.classList.add("is-open");
+  imageOpenModal.src = event.target.dataset.source;
 }
-const myGame = new Game();
 
-const p = document.createElement("p");
-p.textContent = `I like ${myGame.name}.`;
+function closeModalClick(event) {
+  modal.classList.remove("is-open");
+  imageOpenModal.src = "";
+}
 
-const heading = document.createElement("h1");
-heading.textContent = "Interesting";
+function onBackDropClick(event) {
+  if (event.target !== overlayRef) {
+    return;
+  }
+  closeModalClick();
+}
 
-const app = document.querySelector("#root");
-app.append(heading, p);
-console.log("Privet");
+function closeModalClickEscape(event) {
+  if (event.key !== "Escape") {
+    return;
+  }
+  closeModalClick();
+}
